b814e4c115fc1822ac6a2a4d64bbe8d204d92236
quyennv <quyen.nguyenvan@tapbi.vn>
2025-12-17 08:56:19 +0700

    update train colab

--- scripts/train_yolo.py
+++ scripts/train_yolo.py
@@ -4,12 +4,20 @@ DATASET_PATH = "configs/tesla_dataset.yaml"
 
 model = YOLO("yolo11n.pt")
 
-
 model.train(
     data=DATASET_PATH,
     imgsz=640,
-    epochs=50,
-    batch=4,
-    device="cpu",
-    name="tesla_y11n_cpu"
+    epochs=100,
+    batch=16,
+    device=0,          # GPU
+    name="tesla_y11n_gpu"
 )
+
+# model.train(
+#     data=DATASET_PATH,
+#     imgsz=640,
+#     epochs=50,
+#     batch=4,
+#     device="cpu",
+#     name="tesla_y11n_cpu"
+# )
